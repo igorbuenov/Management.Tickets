682b4c54afeaa78575713529c689656e668e27b9
Igor Bueno Ventura <ib_ventura@hotmail.com>
2026-09-10 13:35:13 -0300

    Add Validator

---
+++ src/backend/Tickets.Application/Validators/Password/ResetPasswordRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Tickets.Application.Commons.Security;
+using Tickets.Application.DTOs.Auth;
+
+namespace Tickets.Application.Validators.Password
+{
+    public class ResetPasswordRequestValidator : AbstractValidator<ResetPasswordRequestDto>
+    {
+
+        public ResetPasswordRequestValidator()
+        {
+            RuleFor(x => x.NewPassword)
+                .NotEmpty()
+                .WithMessage("New password cannot be empty.")
+                .Length(PasswordPolicy.MinLength, PasswordPolicy.MaxLength)
+                .WithMessage($"New password must contain between {PasswordPolicy.MinLength} and {PasswordPolicy.MaxLength} characters.")
+                .Matches(PasswordPolicy.Regex)
+                .WithMessage(PasswordPolicy.DefaultValidationMessage)
+                .Must(password => !password.Any(char.IsWhiteSpace))
+                .WithMessage("New password cannot contain spaces.");
+        }
+
+        
+    }
+}
